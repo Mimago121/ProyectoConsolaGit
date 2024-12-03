ec665e9b120e7516af16aaa7008209ea49dfd77e
Mimago121 <miriammartinezgonzalez2001@gmail.com>
2024-12-03 18:52:25 +0100

    Creado archivo MensajeOperaciones.java

---
+++ src/MensajeOperaciones.java
@@ -0,0 +1,19 @@
+public class MensajeOperaciones {
+    public static void saludar (){
+
+        System.out.println("Buenos dias");
+        
+    }
+    public static void despedida (){
+
+        System.out.println("Adiós");
+    }
+    
+    public static void main (String [] args){
+    
+        saludar();
+        despedida();
+     
+    }
+}
+
